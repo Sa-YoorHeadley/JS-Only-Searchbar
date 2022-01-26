b837ddbad500db0747ddec6e380ac97bbd7c1edd
Sa Yoor Headley <sai.h152015@gmail.com>
2022-01-25 20:12:59 -0400

    Final Commit

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const characterList = document.getElementById("character-list")
+const searchBar = document.getElementById("search-bar")
 let characters = []
+
+searchBar.addEventListener('input', () =>{
+    const searchKey = searchBar.value.toLowerCase()
+
+    const matchedCharacters = characters.filter(character =>{
+        return character.name.toLowerCase().includes(searchKey) || 
+        character.house.toLowerCase().includes(searchKey) || 
+        character.patronus.toLowerCase().includes(searchKey)
+    })
+    console.log(matchedCharacters) 
+    displayCharacters(matchedCharacters)
+})
 
 const loadCharacters = async () =>{
     try{
         const res = await fetch("http://hp-api.herokuapp.com/api/characters")
         characters = await res.json()
-        console.log(characters)
+        characters = characters.filter(character =>{
+            return character.image != ""
+        })
         displayCharacters(characters)
     }
     catch(error){
@@ -14,14 +29,13 @@ const loadCharacters = async () =>{
 }
 
 function displayCharacters(characters){
-    characters = characters.filter(character =>{
-        return character.image != ""
-    })
+    characterList.innerHTML = ''
+    
     characters.forEach(character =>{
-
+        
         let listItem = document.createElement("li")
         listItem.classList.add("character")
-
+        
         let name = document.createElement("h2")
         name.classList.add("name")
         name.innerText = `Name: ${character.name}`
@@ -37,13 +51,16 @@ function displayCharacters(characters){
         let image = document.createElement("img")
         image.classList.add("image")
         image.setAttribute("src", character.image)
-
+        
         listItem.append(name)
         listItem.append(house)
         listItem.append(patronus)
         listItem.append(image)
-
+        
         characterList.append(listItem)
     })
 }
+
+
+
 loadCharacters()
